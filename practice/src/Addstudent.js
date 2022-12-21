0a18061be29c3197d9e096bf8a35066f43d68029
vijayakumar58 <vijayalingam58@gmail.com>
2022-12-21 12:39:06 +0530

    add change

---
+++ practice/src/Addstudent.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import React,{ Component } from 'react'
+import Header from './title/Header'
+import './Addstudent.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
