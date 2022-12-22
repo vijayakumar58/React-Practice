c9e07720035125969bdfd5b49ab3c4db2f08eac1
vijayakumar58 <vijayalingam58@gmail.com>
2022-12-22 10:39:41 +0530

    add js

--- practice/src/Addstudent.js
+++ practice/src/Addstudent.js
@@ -45,21 +45,13 @@ export class Addstudent extends Component {
         <div className='component-body-container'>
           <button className='custom-btn'>Back</button>
           <form className='student-form' onSubmit={this.onFormSubmitted.bind(this)}>
-            <label htmlFor='studentname' className='label-control'>Name: </label> <label htmlFor='parentname' className='label-control'>Parent Name: </label><br />
-            <input id="studentname" type="text" className="input-control" name="studentname" onChange={this.inputChangeHandler}></input> <input id="parentname" type="text" className="input-control" name="parentname" onChange={this.inputChangeHandler}></input><br /><br />
-            <label htmlFor='address' className='label-control'>Address: </label> <label htmlFor='date of birth' className='label-control'>Date Of Birth: </label><br />
-            <input id="address" type="text" className="input-control" name="address" onChange={this.inputChangeHandler}></input> <input id="date of birth" type="text" className="input-control" name="date of birth" onChange={this.inputChangeHandler}></input><br /><br />
+            <span><label htmlFor='studentname' className='label-control'>Name: </label><input id="studentname" type="text" className="input-control" name="studentname" onChange={this.inputChangeHandler}></input></span>     <span><label htmlFor='parentname' className='label-control'>Parent Name: </label><input id="parentname" type="text" className="input-control" name="parentname" onChange={this.inputChangeHandler}></input></span><br /><br />
+             <span><label htmlFor='address' className='label-control'>Address: </label><input id="address" type="text" className="input-control" name="address" onChange={this.inputChangeHandler}></input></span><span><label htmlFor='date of birth' className='label-control'>Date Of Birth: </label><input id="date of birth" type="text" className="input-control" name="date of birth" onChange={this.inputChangeHandler}></input></span><br /><br />
             <p>Subject Marks Out Of 100</p>
-            <label htmlFor='language' className='label-control'>Language: </label> <label htmlFor='english' className='label-control'>English: </label><br />
-            <input id="language" type="text" className="input-control" name="language" onChange={this.inputChangeHandler}></input> <input id="english" type="text" className="input-control" name="english" onChange={this.inputChangeHandler}></input><br /><br />
-            <label htmlFor='maths' className='label-control'>Maths: </label> <label htmlFor='science' className='label-control'>Science: </label><br />
-            <input id="matha" type="text" className="input-control" name="maths" onChange={this.inputChangeHandler}></input> <input id="science" type="text" className="input-control" name="science" onChange={this.inputChangeHandler}></input><br /><br />
-            <label htmlFor='social science' className='label-control'>Social science: </label> <label htmlFor='total marks' className='label-control'>Total Marks: </label><br />
-            <input id="social science" type="text" className="input-control" name="social science" onChange={this.inputChangeHandler}></input> <input id="total marks" type="text" className="input-control" name="total marks" onChange={this.inputChangeHandler}></input><br /><br />
-            <label htmlFor='percentage of mark' className='label-control'>Percentage of Mark: </label><br />
-            <input id="percentage of mark" type="text" className="input-control" name="percentage of mark" onChange={this.inputChangeHandler}></input><br /><br />
-            <label htmlFor='select your group' className='label-control'>Select Your Group: </label><br />
-            <select name="select your group" id="select your group">
+            <span><label htmlFor='language' className='label-control'>Language: </label><input id="language" type="text" className="input-control" name="language" onChange={this.inputChangeHandler}></input></span><span><label htmlFor='english' className='label-control'>English: </label><input id="english" type="text" className="input-control" name="english" onChange={this.inputChangeHandler}></input></span><br /><br />
+            <span><label htmlFor='maths' className='label-control'>Maths: </label><input id="matha" type="text" className="input-control" name="maths" onChange={this.inputChangeHandler}></input></span><span><label htmlFor='science' className='label-control'>Science: </label><input id="science" type="text" className="input-control" name="science" onChange={this.inputChangeHandler}></input></span><br /><br />
+            <span><label htmlFor='social science' className='label-control'>Social science: </label><input id="social science" type="text" className="input-control" name="social science" onChange={this.inputChangeHandler}></input></span><span><label htmlFor='total marks' className='label-control'>Total Marks: </label><input id="total marks" type="text" className="input-control" name="total marks" onChange={this.inputChangeHandler}></input></span><br /><br />
+            <span><label htmlFor='percentage of mark' className='label-control'>Percentage of Mark: </label><input id="percentage of mark" type="text" className="input-control" name="percentage of mark" onChange={this.inputChangeHandler}></input></span><span><label htmlFor='select your group' className='label-control'>Select Your Group: </label><select name="select your group" id="select your group">
                    <option value="Maths Biology">Maths Biology</option>
                    <option value="Computer Maths">Computer Maths</option>
                    <option value="Agri">Agri</option>
@@ -67,11 +59,9 @@ export class Addstudent extends Component {
                    <option value="History">History</option>
                    <option value="Science">Science</option>
                    <option value="Vocational">Vocational</option>
-            </select><br /><br />
-            <label htmlFor='emai id' className='label-control'>Email Id: </label> <label htmlFor='mobile number' className='label-control'>Mobile Number: </label><br />
-            <input id="email id" type="text" className="input-control" name="email id" onChange={this.inputChangeHandler}></input> <input id="mobile number" type="text" className="input-control" name="mobile number" onChange={this.inputChangeHandler}></input><br /><br />
-           
-            <div className='student-info-container'>
+            </select></span><br /><br />
+            <span><label htmlFor='emai id' className='label-control'>Email Id: </label><input id="email id" type="text" className="input-control" name="email id" onChange={this.inputChangeHandler}></input></span><span><label htmlFor='mobile number' className='label-control'>Mobile Number: </label><input id="mobile number" type="text" className="input-control" name="mobile number" onChange={this.inputChangeHandler}></input></span><br /><br />
+           <div className='student-info-container'>
               <span className='student-to-add-heading'>Student to be added:</span><br />
               <span className='student-info'>StudentName: {studentname}</span><span className='student-info'>Parent Name: {parentname}</span><br />
               <span className='student-info'>Address: {address}</span><span className='student-info'>Date Of Birth: {dateofbirth}</span><br />
